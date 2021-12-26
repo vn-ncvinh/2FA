71906ab0f686404ff9cbd321a96080845113165e
Nguyen Cong Vinh <80946737+vn-ncvinh@users.noreply.github.com>
2021-12-26 15:20:45 +0700

    move javascript to file

---
+++ js/otp.js
@@ -0,0 +1,33 @@
+var totp = new jsOTP.totp();
+var secret = document.getElementById("secret").value;
+
+function copyText() {
+    var copyText = totp.getOtp(secret);
+    navigator.clipboard.writeText(copyText);
+}
+
+function updatSecret() {
+    secret = document.getElementById("secret").value.replaceAll(" ", "");
+    updateTotp();
+}
+
+function updateTicker(tick) {
+    document.getElementById("ticker").innerText = "Expires later: " + tick + " second";
+
+}
+
+function updateTotp() {
+    document.getElementById("code-1").innerText = totp.getOtp(secret);
+}
+
+updateTotp();
+
+function timeLoop() {
+    var epoch = Math.round(new Date().getTime() / 1000.0);
+    var countDown = 30 - (epoch % 30);
+    document.getElementById("timecountdown").value = 30 - countDown;
+    updateTicker(countDown);
+    if (epoch % 30 == 0) updateTotp();
+}
+
+setInterval(timeLoop, 1000);
